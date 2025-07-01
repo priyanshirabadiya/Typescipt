25413b77afbb7a066b3ed9d3713eae1e6faaf6d5
Priyanshi Rabadiya <rabadiyapriyansi@gmail.com>
2025-07-02 00:43:58 +0530

    -

---
+++ Tutorial/typeinference.ts
@@ -0,0 +1,22 @@
+// inference = means guess by it self like if we don't provide any type at that time it automatically consider its type
+
+// let a = 22;
+// let b = "sdf";
+// let c = true;
+
+// union & intersection types
+
+// let sname : string | null;
+// sname.   it will show all methods of string and null
+
+// function abcd(variable: string | number): string | number {
+//   if (typeof variable === "number") {
+//     return Number(variable.toFixed(2));
+//   } else {
+//     return variable.toLocaleUpperCase();
+//   }
+// }
+
+// console.log(abcd(121));
+// console.log(abcd(121.12));
+// console.log(abcd("Hello"));
