60d1f67509297af382f3a47c70334d816e9977aa
Priyanshi Rabadiya <rabadiyapriyansi@gmail.com>
2025-07-02 00:17:03 +0530

    -

---
+++ Tutorial/script.js
@@ -0,0 +1,19 @@
+// var a = 12;
+
+let a = 10;
+let b = a;
+
+a = a + 2;
+console.log(a);
+console.log(b);
+
+
+
+let arr1 = [10,20,30];
+let arr2 = arr1;
+
+arr2.pop();
+
+console.log(arr2);
+console.log(arr1);
+
---
+++ Tutorial/script.ts
@@ -0,0 +1,105 @@
+// var a = 12;
+
+// types
+// primitive reference
+
+// Basic types
+// number , String , Boolean
+// arrays , tuples
+// Any , Unknown , Never, Void
+// Enums
+
+// unknown 
+// Use **any** when you want no type checking — but it's risky.
+// Use **unknown** when the type is dynamic, but you still want safety.
+// unknown is the right choice — especially when working with dynamic input such as JSON from an API, user input, or a third-party library.
+
+// Why Use unknown Here?
+// JSON.parse returns any by default, which is unsafe.
+// Changing its return type to unknown forces you to check types manually, avoiding runtime errors.
+
+// function handleInput(input: unknown) {
+//   if (typeof input === "string") {
+//     console.log("Uppercase:", input.toUpperCase());
+//   } else if (typeof input === "number") {
+//     console.log("Fixed:", input.toFixed(2));
+//   } else {
+//     console.log("Unknown input type.");
+//   }
+// }
+
+// handleInput("hello");
+// handleInput(42);
+// handleInput(true); // "Unknown input type."
+
+
+// never = it means it will never returns anything like in this it will infinitly print but never return
+// any thing  -- in short this function will never return any thing everrr
+// function abcd() : never {
+//     while(true){
+//          console.log("infinite run");
+//     }
+// }
+// abcd()
+
+
+
+
+// let arr: [number, string] = [1, 2, 3, 4, "harsh"];
+
+// let names: string[] = ["Alice", "Bob"];
+// let numbers: number[] = [10,20,30];
+
+// console.log(typeof names);
+
+// interface User{
+//     name : string,
+//     rno : number
+// }
+
+// let User : User = {
+//     name : "priyanshi",
+//     rno : 12
+// }
+
+// console.log(User);
+
+// type Address = {
+//   city: string;
+//   zip: string;
+// };
+
+// type User = {
+//   name: string;
+//   address: Address;
+// };
+
+// const user: User = {
+//   name: "Dave",
+//   address: {
+//     city: "New York",
+//     zip: "10001"
+//   }
+// };
+
+// interface Address {
+//   city: string;
+//   hnumber: number;
+// }
+
+// interface EmpDetails {
+//   ename: string;
+//   eno: number;
+//   address: Address;
+// }
+
+// const user: EmpDetails = {
+//   ename: "Hello",
+//   eno: 12,
+//   address: {
+//     city: "surat",
+//     hnumber : 12
+//   },
+// };
+
+// console.log(user);
